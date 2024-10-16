c057e10dd71b27d8d50b2da9a68704c8dcffc3c3
Chebannich <chieban.stas@gmail.com>
2024-10-17 00:25:19 +0200

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let revArray = [];
+  let array = [];
+
+  if (n > 0) {  // if "n" is above 0
+    array = ('' + n).split('');
+
+    for (const digit of array) {
+      revArray.unshift(digit);
+    }
+
+    return revArray.join('')
+
+  } else if (n < 0) {  // if "n" is less 0
+    array = (-n + '').split('');
+
+    for (const digit of array) {
+      revArray.unshift(digit);
+    }
+
+    return revArray.join('');
+
+  } else {  // if "n" is 0
+    return 0;
+  }
 }
