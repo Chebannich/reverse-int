ae2e2d3a7157fa1a44e2e4562936f9b766e6fb4b
Stanislav <58264863+Chebannich@users.noreply.github.com>
2024-10-19 18:07:32 +0200

    refactor: add comments

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 module.exports = function reverse(n) {
-  let revArray = [];
-  let array = [];
+  let revArray = []; // Array to store the reversed digits
+  let array = [];    // Array to store the digits of the number
 
-  if (n > 0) {  // if "n" is above 0
+  // If the number "n" is positive
+  if (n > 0) {
+    // Convert the number to a string, then split it into an array of digits
     array = ('' + n).split('');
 
+    // Reverse the digits by adding each digit to the front of revArray
     for (const digit of array) {
       revArray.unshift(digit);
     }
 
-    return revArray.join('')
-
-  } else if (n < 0) {  // if "n" is less 0
-    array = (-n + '').split('');
-
-    for (const digit of array) {
-      revArray.unshift(digit);
-    }
-
+    // Join the reversed digits into a string and return it
     return revArray.join('');
 
-  } else {  // if "n" is 0
-    return 0;
+  // If the number "n" is negative
+  } else if (n < 0) {
+    // Convert the absolute value of the number to a string, then split it into digits
+    array = (-n + '').split('');
+
+    // Reverse the digits by adding each digit to the front of revArray
+    for (const digit of array) {
+      revArray.unshift(digit);
+    }
+
+    // Join the reversed digits into a string and return it
+    return revArray.join('');
+
+  // If the number "n" is zero
+  } else {
+    return 0;  // Simply return 0
   }
 }
